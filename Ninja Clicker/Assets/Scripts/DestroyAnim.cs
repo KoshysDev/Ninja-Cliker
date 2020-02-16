26b33d3a488537d52ecaeffc204fa02d756a83e3
KoshysDev <koshys.osu@gmail.com>
2020-02-16 16:02:09 +0200

    Added hit animation

---
+++ Ninja Clicker/Assets/Scripts/DestroyAnim.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAnim : MonoBehaviour
+{
+    public float timer = 1.0f;
+    private bool S = false;
+
+    public void DestroyMe()
+    {
+        Destroy(this.gameObject);
+    }
+
+    private void Start()
+    {
+        S = true;
+    }
+
+    private void Update()
+    {
+        if (S == true && timer >= 0f)
+        {
+            timer -= 0.1f;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+}
